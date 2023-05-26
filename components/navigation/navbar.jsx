e9185b7b57d17be70f61f1aed11550b76848a4ab
Sudeer Kamat <sudeerkamat@gmail.com>
2023-05-26 17:31:30 +0530

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<a href="https://chrome.google.com/webstore/detail/revamp-for-salesforce/llgcejoamphenmjaoiipcipcohjpmjof" target={"_blank"}>
-				<img className={styles.alchemy_logo} src="/public/20230515_111544-128.png"></img>
+				<img className={styles.alchemy_logo} src="/20230515_111544-128.png"></img>
 			</a>
 		</nav>
 	);
