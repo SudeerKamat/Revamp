d35054d8520c115dadb9158b5d12188d4ef8cd22
Sudeer Kamat <sudeerkamat@gmail.com>
2023-05-26 17:13:16 +0530

    Update InstructionsComponent.jsx

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -6,11 +6,11 @@ export default function InstructionsComponent() {
 		<div className={styles.container}>
 			<header className={styles.header_container}>
 				<h1>
-					create<span>-web3-dapp</span>
+					Revamp<span>For Salesforce</span>
 				</h1>
 				<p>
-					Get started by editing this page in{" "}
-					<span>/pages/index.js</span>
+					Revamp Your Salesforce
+					<span>Experience</span>
 				</p>
 			</header>
 
