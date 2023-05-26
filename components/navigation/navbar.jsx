f8b15ac920e2e8220445ab8c7c52f561dc5d2e89
Sudeer Kamat <sudeerkamat@gmail.com>
2023-05-26 17:28:27 +0530

    Update navbar.jsx

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -3,10 +3,9 @@ import styles from "../../styles/Navbar.module.css";
 export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
-			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
+			<a href="https://chrome.google.com/webstore/detail/revamp-for-salesforce/llgcejoamphenmjaoiipcipcohjpmjof" target={"_blank"}>
+				<img className={styles.alchemy_logo} src="/public/20230515_111544-128.png"></img>
 			</a>
-			<ConnectButton></ConnectButton>
 		</nav>
 	);
 }
